4807a21b2f2b55d26320a9816e72f22c10b8e5a5
Harisa Saeed <155890055+harisasaeed@users.noreply.github.com>
2024-03-30 12:54:32 -0700

    Update main.ts

--- Exercise 41/main.ts
+++ Exercise 41/main.ts
@@ -1,5 +1,9 @@
 //Task 41
 
+//Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+
+
+
 let magicians: string[] = ["Alice", "David", "Chris"];
 function show_magicians(magicians: string []){
     magicians.forEach(magicians => {
