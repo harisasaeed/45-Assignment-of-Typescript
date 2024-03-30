305f1c4ef1b25ebfde6c69817d04178deef330b8
Harisa Saeed <155890055+harisasaeed@users.noreply.github.com>
2024-03-30 12:51:04 -0700

    Update main.ts

--- Exercise 37/main.ts
+++ Exercise 37/main.ts
@@ -1,5 +1,10 @@
 //Task 37
 
+//T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt.
+// The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+
+
 function make_shirt(size: string = "large",message: string = "I love typescript."){
     console.log(`Making a ${size} t_shirt with the message "${message}" Printed on it.`);
 }
