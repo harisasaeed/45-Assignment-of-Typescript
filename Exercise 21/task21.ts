cbe08edcba05d8383a7075437a47ea4e54bed52d
Harisa Saeed <155890055+harisasaeed@users.noreply.github.com>
2024-03-30 12:33:03 -0700

    Update task21.ts

--- Exercise 21/task21.ts
+++ Exercise 21/task21.ts
@@ -1,5 +1,7 @@
 //Task 21
 
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
 let book:{title: string; author: string; yearPublished: number} =
 {
     title: "The Hobbit",
